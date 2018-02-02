aa2399a91ecf3255a7c735739128726ea8f7667d
Alexander Kholodovitch <kholodovitch@gmail.com>
2018-02-02 12:59:16 +0300

    Add Push hook example.

--- src/main/java/com/kholodovitch/telegram/gitlab/handlers/HttpHandler.java
+++ src/main/java/com/kholodovitch/telegram/gitlab/handlers/HttpHandler.java
@@ -2,6 +2,7 @@ package com.kholodovitch.telegram.gitlab.handlers;
 
 import java.io.IOException;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpException;
@@ -12,6 +13,7 @@ import org.apache.http.protocol.HttpRequestHandler;
 import org.apache.http.util.EntityUtils;
 import org.telegram.telegrambots.logging.BotLogger;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kholodovitch.telegram.gitlab.JacksonConfig;
 import com.kholodovitch.telegram.gitlab.hook.model.PipelineHook;
 
@@ -30,10 +32,32 @@ public class HttpHandler implements HttpRequestHandler {
 			return;
 
 		try {
+			ObjectMapper objectMapper = JacksonConfig.getObjectMapper();
 			HttpEntity entity = ((HttpEntityEnclosingRequest) request).getEntity();
 			String body = EntityUtils.toString(entity);
-			PipelineHook hook = JacksonConfig.getObjectMapper().readValue(body, PipelineHook.class);
-			bot.handle(hook);
+
+			Header event = request.getFirstHeader("X-Gitlab-Event");
+			if (event == null)
+				return;
+
+			switch (event.getValue()) {
+			case "Pipeline Hook":
+				System.out.println("Pipe: " + body);
+				PipelineHook hook = objectMapper.readValue(body, PipelineHook.class);
+				bot.handle(hook);
+				break;
+
+			case "Push Hook":
+				System.out.println("Push: " + body);
+				break;
+
+			case "Tag Push Hook":
+				System.out.println(" Tag: " + body);
+				break;
+
+			default:
+				break;
+			}
 		} catch (Exception e) {
 			BotLogger.severe(LOGTAG, e);
 		}
